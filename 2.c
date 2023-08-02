2b7a0d668f176fdf08e1f5a75e736bb5cc827f2e
snippetsx <snippetsx@lcsa.ru>
2023-08-02 18:35:38 +0300

    added 2.c

---
+++ 2.c
@@ -0,0 +1,22 @@
+// https://codeforces.com/problemset/problem/282/A
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int n;
+    char str[3];
+    int x = 0;
+    scanf ("%d", &n);
+    for (int i = 0; i < n; i++){
+        scanf ("%s", &str);
+        if (strcmp(str, "X++") == 0 || strcmp(str, "++X") == 0){
+            x++;
+        }
+        else if (strcmp(str, "X--") == 0 || strcmp(str, "--X") == 0){
+            x--;
+        }
+
+    }
+    printf("%d", x);
+
+}
