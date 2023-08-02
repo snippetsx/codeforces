dfffd44321a4727966eec56a4929ed39b829b017
snippetsx <snippetsx@lcsa.ru>
2023-08-02 18:26:17 +0300

    added 1.c

---
+++ 1.c
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/50/A
+#include <stdio.h>
+
+int main(){
+    int n;
+    int m;
+    int ans;
+    scanf ("%d" "%d", &n, &m);
+    ans = (n * m) / 2;
+    printf ("%d", ans);
+    
+}
