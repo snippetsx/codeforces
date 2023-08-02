d669c54fecff782c1e323a5d4f9197aea9fdbd8e
snippetsx <snippetsx@lcsa.ru>
2023-08-02 18:44:57 +0300

    added 3.c

---
+++ 3.c
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemset/problem/281/A
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    char str[1000];
+    scanf ("%s", &str);
+    str[0] = toupper(str[0]);
+    printf("%s", str);
+}
