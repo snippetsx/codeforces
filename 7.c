d643aea3662dab90b0579e0ce040ff1a6ae4dfdb
snippetsx <snippetsx@lcsa.ru>
2023-08-02 19:19:39 +0300

    added 7.c

---
+++ 7.c
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/467/A
+#include <stdio.h>
+
+int main(){
+    int n;
+    int m;
+    int t;
+    int cnt = 0;
+    scanf ("%d", &t);
+    for (int i = 0; i < t; i++){
+        scanf ("%d" "%d", &n, &m);
+        if (n + 2 <= m){
+        cnt++;
+ 
+        }
+    }
+    printf ("%d", cnt);
+    return 0;
+}
