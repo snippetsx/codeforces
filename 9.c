630c97cacad2500c5cf871b330e81f961f99629e
snippetsx <snippetsx@lcsa.ru>
2023-08-02 19:40:28 +0300

    added 9.c

---
+++ 9.c
@@ -0,0 +1,17 @@
+// https://codeforces.com/problemset/problem/1030/A
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int n;
+    int v;
+    char state[5] = "EASY";
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        scanf("%d", &v);
+        if (v == 1){
+            strcpy(state, "HARD");
+        }
+    }
+    printf("%s", state);
+}
