4adbb6ee834c0448d00ede318fb3ecbd6baa7b89
snippetsx <snippetsx@lcsa.ru>
2023-08-02 18:53:20 +0300

    added 4.c

--- 4.c
+++ 4.c
@@ -3,5 +3,15 @@
 #include <ctype.h>
 
 int main(){
+    int a;
+    int b;
+    int cnt = 0;
+    scanf("%d" "%d", &a, &b);
+    while (a <= b){
+        b = b * 2;
+        a = a * 3;
+        cnt = cnt + 1;
+    }
+    printf("%d", cnt);
     return 0;
 }
