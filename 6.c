1392e8bf6c468d4dfa62f63199d0090a7b71cbb3
snippetsx <snippetsx@lcsa.ru>
2023-08-02 19:14:21 +0300

    added 6.c

---
+++ 6.c
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/977/A
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    int n;
+    int k;
+    scanf ("%d" "%d", &n, &k);
+    for (int i = 0; i < k; i++){
+        if (n % 10 == 0){
+            n = n / 10;
+        } 
+        else if (n % 10 != 0){
+            n = n - 1;
+        }
+    }
+    printf ("%d", n);
+    return 0;
+}
