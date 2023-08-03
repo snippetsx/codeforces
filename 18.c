4b0cb5fd6e79c50d5dc42eaeb56d2949af4ecc01
snippetsx <snippetsx@lcsa.ru>
2023-08-03 11:36:19 +0300

    added 18.c

--- 18.c
+++ 18.c
@@ -1,7 +1,30 @@
-// PRESET
+// https://codeforces.com/problemset/problem/233/A
 #include <stdio.h>
 #include <ctype.h>
 
 int main(){
+    int n;
+    int e;
+    int o;
+    int i;
+    scanf ("%d", &n);
+    if (n % 2 == 1){
+        printf ("-1");
+    }
+    else {
+        e = 2;
+        o = 1;
+        for(i = 1; i <= n; i++) {
+            if(i % 2 == 0) {
+                printf ("%d ", o);
+                o = o + 2;
+            }
+            else
+            {
+                printf ("%d ", e);
+                e = e + 2;
+            }
+        }
+    }
     return 0;
 }
