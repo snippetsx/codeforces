834564dc4168ced5f8a7e60e9f0429d09c48f5ba
snippetsx <snippetsx@lcsa.ru>
2023-08-12 10:32:40 +0300

    improved codestyling, added gitignore

--- Olymps/ВСОШ_СИРИУС_ПРИГЛАС_ЭТАП/b.c
+++ Olymps/ВСОШ_СИРИУС_ПРИГЛАС_ЭТАП/b.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int tv_channels(int p, int u, int k){
+int tv_channels(int p, int u, int k) {
     int t1;
     int t2;
     int d;
     int a1;
     int a2;
     int lp;
-    if (p > u){
+    if (p > u) {
         t1 = p;
         t2 = u;
         u = t1;
@@ -19,36 +19,37 @@ int tv_channels(int p, int u, int k){
     lp = d / k;
     a1 = lp + d % k;
     a2 = (lp + 1) + (k - d % k);
-    if (a1 < a2){
+    if (a1 < a2) {
         return a1;
     }
-    else{
-        return a2;
-    }
+    return a2;
 }
-int min(int a, int b, int c){
-    int x = 10000000;
-    if(x > a){
-        x = a;
+
+int min(int a, int b) {
+    if(a > b) {
+        return b;
     }
-    if(x > b){
-        x = b;
-    }
-    if(x > c){
-        x = c;
-    }
-    return x;
+    return a;
+}
+
+int min3(int a, int b, int c) {
+    return min(min(a, b), c);
 }
 
 
-int main(){
+int main() {
     int n;
     int p;
     int k;
     int u;
     int cnt;
+
     scanf("%d" "%d" "%d" "%d", &n, &k, &p, &u);
-    cnt = min(tv_channels(p, u, k), (p - 1 + k - 1) / k + tv_channels(1, u, k), (n - p + k - 1) / k + tv_channels(n, u, k));
+
+    cnt = min3(tv_channels(p, u, k), 
+               (p - 1 + k - 1) / k + tv_channels(1, u, k), 
+               (n - p + k - 1) / k + tv_channels(n, u, k));
+
     printf("%d", cnt);
     return 0;
 }
